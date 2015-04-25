126b3841d33b72911a671a0c9c9825b2165b457e
Chino <chinomask@gmail.com>
2015-04-25 21:06:29 +0800

    Passed
    换了个笨办法 通过PAT了...

--- SpringDataStructPAT/004.cpp
+++ SpringDataStructPAT/004.cpp
@@ -2,110 +2,105 @@
 
 using namespace std;
 
+bool isFunNum(const int *a, int lenA, const int *b, int lenB)
+{
+  if(lenA != lenB) return false;
+  for(int i = 0; i < 10; ++i)
+  {
+	if(a[i] != b[i])
+	{
+	  return false;
+	}
+  }
+  return true;
+}
+
+int* dNum(int *a, int len)
+{
+  int *p;
+  int lenP = len;
+  if(a[0] > 4) lenP++;
+  p = new int [lenP];
+  
+  bool flag = false;
+  for(int i = len-1, j = lenP-1; i >= 0; --i, --j)
+  {
+  	p[j] = a[i] * 2 % 10;
+    if(flag)
+    {
+      p[j]++;
+      flag = false;
+    }
+    if(a[i] > 4) flag = true;
+  }
+  if(lenP > len) p[0] = 1;
+  
+  return p;
+}
+
 int ctoi(char c)
 {
-	if(c > '/' && c <':') return c-'0';
+  if(c > '/' && c <':') return c-'0';
 }
-
-int* multiBy2(int *num, const int len)
-{
-	int *res;
-	int resLen = len;
-	if(num[0]>4)
-	{
-		resLen++;
-		res = new int [resLen];
-	}
-	else
-		res = new int [resLen];
-
-	bool flag = false;
-	for(int i = resLen-1, j = len-1; j >= 0; --i,--j)
-	{
-		res[i] = num[j] * 2 % 10;
-		if(flag)
-		{
-			res[i]++;
-			flag = false;
-		}
-		if(num[j]>4) flag = true;
-	}
-	if(resLen>len) res[0] = 1;
-	
-	return res;
-	
-}
-
-bool isFunNum(int *num, int *res, int len)
-{
-	int prod = 0;
-	bool flag = false;
-	int zNumN = 0, zNumR = 0;
-	for(int i = 0; i < len; ++i)
-	{
-		if(num[i] == 0) zNumN++;
-		if(res[i] == 0) zNumR++;
-		if(res[i]) flag = true;
-		prod = num[i]^res[i]^prod;
-	}
-	if((!prod) && flag && (zNumN == zNumR)) return true;
-	else return false;
-}
-
 
 int main()
 {
-	char cnum[20];
-	cin.get(cnum,21);
-		
-	int num[20];
-	int len = 0;
-	for(; cnum[len] != '\0'; ++len)
-	{
-		num[len] = ctoi(cnum[len]);
+  char cnum[20];
+  cin.get(cnum,21);
+  int num[20];
+  int len = 0;
+  
+  bool zeroFlag = true;
+  for(int i = 0; cnum[i] != '\0'; ++i)
+  {
+  	if(cnum[i] != '0') zeroFlag = false;
+  	if(zeroFlag)
+  	{
+  	  continue;
 	}
+	num[len] = ctoi(cnum[i]);
+	len++;
+  }
 
-	int *res = multiBy2(num, len);
-	int resLen = len;
-	bool eqLen = true;
-	if(num[0] > 4)
-	{
-		resLen++;
-		eqLen = false;
-	}
-	if(eqLen)
-	{
-		if(isFunNum(num,res,len))
-		{
-			cout << "Yes" << endl;
-		}
-		else
-		{
-			cout << "No" << endl;
-		}
-	}
-	else
-	{
-		cout << "No" << endl;
-	}
-	
-	
-	bool flag = false;
-	for(int i = 0; i < resLen; ++i)
-	{
-		if(!flag)
-		{
-			if(res[i] != 0) flag = true;
-		}
-		if(flag)
-		{
-			cout << res[i];
-		}
-	}
-	if(!flag) cout << 0;
-	cout << endl;
-	
-	delete [] res;
-	
-	return 0;
+  int counterA[10];
+  for(int i = 0; i < 10; ++i)
+  {
+  	counterA[i] = 0;
+  }
+  for(int i = 0; i < len; ++i)
+  {
+  	counterA[num[i]]++;
+  }
+  
+  int lenB = (num[0]>4)?(len+1):len; 
+  int *douNum = dNum(num, len);
+  int counterB[10];
+  for(int i = 0; i < 10; ++i)
+  {
+  	counterB[i] = 0;
+  }
+  for(int i = 0; i < lenB; ++i)
+  {
+  	counterB[douNum[i]]++;
+  }
+
+  if(isFunNum(counterA, len, counterB, lenB))
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+  
+  for(int i = 0; i < lenB; ++i)
+  {
+    cout << douNum[i];
+  }
+  cout << endl;
+ 
+  delete [] douNum;
+  return 0;
 }
+
+
